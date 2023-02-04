54e5372fd5a29eea665aad635f1ff27df74218b3
lion18790829 <saroolionet81@gmail.com>
2023-02-05 03:00:24 +0900

    20130205
    init project

---
+++ src/main/java/com/globalit/GlobalitApplication.java
@@ -0,0 +1,13 @@
+package com.globalit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalitApplication.class, args);
+	}
+
+}
